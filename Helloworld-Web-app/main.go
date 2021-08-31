fb15fb02af004adaba34fef223f054670fd695b3
Erik Seyti <erikseyti@gmail.com>
2021-08-31 11:16:11 -0300

    Create separate functions to handle with the respective pages with a Golang server

--- Helloworld-Web-app/main.go
+++ Helloworld-Web-app/main.go
@@ -5,16 +5,30 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+// addValues adds two integers and returns the sumn
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the main application function
 func main() {
-	// fmt.Println("Hello World")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello World!")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written %d", n))
-	})
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
-	_ = http.ListenAndServe(":8080", nil)
+	_ = http.ListenAndServe(portNumber, nil)
 }
