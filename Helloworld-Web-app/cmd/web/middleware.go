d549d2f78b17af90791e852a0635a805d89f69de
Erik Seyti <erikseyti@gmail.com>
2021-09-01 19:47:58 -0300

    Installing and setting up a session package with Go

--- Helloworld-Web-app/cmd/web/middleware.go
+++ Helloworld-Web-app/cmd/web/middleware.go
@@ -15,16 +15,22 @@ func WriteToConsole(next http.Handler) http.Handler {
 	})
 }
 
+// NoSurf adds CSRF protection to all POST requests
 func NoSurf(next http.Handler) http.Handler {
 	crsfHandler := nosurf.New(next)
 
 	crsfHandler.SetBaseCookie(http.Cookie{
 		HttpOnly: true,
 		Path:     "/",
-		Secure:   false,
+		Secure:   app.InProduction,
 		SameSite: http.SameSiteLaxMode,
 	})
 
 	return crsfHandler
 
+}
+
+// SessionLoad loads and saves the session on every request
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
 }
