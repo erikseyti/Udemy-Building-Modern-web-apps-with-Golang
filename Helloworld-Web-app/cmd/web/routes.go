14807fbb77c969692619ca6e5189ddd1595881b2
Erik Seyti <erikseyti@gmail.com>
2021-09-01 19:26:45 -0300

    Developing our own Middleware

--- Helloworld-Web-app/cmd/web/routes.go
+++ Helloworld-Web-app/cmd/web/routes.go
@@ -13,6 +13,8 @@ func routes (app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	// mux.Use(WriteToConsole)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
