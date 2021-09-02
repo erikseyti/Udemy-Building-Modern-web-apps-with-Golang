118966ca03f394d0b5ec059745e35b91bfc9a641
Erik Seyti <erikseyti@gmail.com>
2021-09-02 13:27:18 -0300

    Create new project of booking rooms in a hotel

---
+++ bookings/cmd/web/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/erikseyti/bookings/pkg/config"
+	"github.com/erikseyti/bookings/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"net/http"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+		// mux.Use(WriteToConsole)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
