c888ac02b26d480c67659d350498751cb4a48efa
Erik Seyti <erikseyti@gmail.com>
2021-09-03 11:08:52 -0300

    Using the Html pages created on last lectures, create golang html templates for a monolith app

--- bookings/cmd/web/routes.go
+++ bookings/cmd/web/routes.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/erikseyti/bookings/pkg/config"
 	"github.com/erikseyti/bookings/pkg/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
@@ -13,11 +14,17 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
-		// mux.Use(WriteToConsole)
+	// mux.Use(WriteToConsole)
 	mux.Use(SessionLoad)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
+
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
