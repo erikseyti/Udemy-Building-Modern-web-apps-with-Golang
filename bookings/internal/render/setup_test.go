9cb592e168ba79cd2ed5dc9540fa9a7f97ca852d
Erik Seyti <erikseyti@gmail.com>
2021-09-06 13:35:59 -0300

    Create tests to our Render package - part 2

--- bookings/internal/render/setup_test.go
+++ bookings/internal/render/setup_test.go
@@ -36,3 +36,21 @@ func TestMain(m *testing.M) {
 
 	os.Exit(m.Run())
 }
+
+type myWriter struct{
+
+}
+
+func (tw *myWriter) Header() http.Header {
+	var h http.Header
+	return h
+}
+
+func (tw *myWriter) WriteHeader(i int) {
+
+}
+
+func (tw *myWriter) Write( b []byte) (int, error){
+	lenth :=len(b)
+	return lenth, nil
+} 
